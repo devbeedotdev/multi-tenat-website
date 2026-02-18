aa5509fc70fea408ad46cb97674a20cf4867a079
Abolaji Ayeni <ayenibolaji07@gmail.com>
2026-02-18 17:34:06 +0100

    removed unused code

--- app/[domain]/VairantAPage.tsx
+++ app/[domain]/VairantAPage.tsx
@@ -1,25 +1,4 @@
-// import { ProductCardA } from "@/components/cards/ProductCardA";
-// import VariantAHeader from "@/components/headers/VariantAHeader";
-// import { products } from "@/lib/mock-db";
-// import { TenantPageProps } from "@/types/tenant";
 
-// export default function VariantAPage({ tenant }: TenantPageProps) {
-//   return (
-//     <main className="min-h-screen bg-white px-4 py-3">
-//       <section className="mx-auto max-w-6xl space-y-6">
-//         <header className="space-y-2">
-//           <VariantAHeader tenant={tenant} />
-//         </header>
-
-//         <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-//           {products.map((product) => (
-//             <ProductCardA key={product.productId} product={product} />
-//           ))}
-//         </div>
-//       </section>
-//     </main>
-//   );
-// }
 
 import { ProductCardA } from "@/components/cards/ProductCardA";
 import VariantAHeader from "@/components/headers/VariantAHeader";
