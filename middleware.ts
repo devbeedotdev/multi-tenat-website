acc5efbd617466f11323279e31b03b6e67979739
Abolaji Ayeni <ayenibolaji07@gmail.com>
2026-03-06 19:45:26 +0100

    fixed issue on not found

--- middleware.ts
+++ middleware.ts
@@ -90,5 +90,8 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|images).*)"],
+  matcher: [
+    // "/((?!_next/static|_next/image|favicon.ico|images).*)"
+    "/((?!api|_next/static|_next/image|favicon.ico|images).*)",
+  ],
 };
